056e1cdd212dd8ecd4e61cdd9c8bffe5dfde01ae
guilhermedelucas <guilhermedelucas@yahoo.com>
2017-03-14 09:14:35 +0100

    Change the store to production mode. Deplying on heroku

--- src/store.js
+++ src/store.js
@@ -3,6 +3,8 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 
-const middleware = applyMiddleware(thunk, logger());
+// const middleware = applyMiddleware(thunk, logger()); // use for dev mode
+const middleware = applyMiddleware(thunk); // use for production mode
+
 
 export default createStore(reducer, middleware);
